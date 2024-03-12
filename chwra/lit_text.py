baffd10babd6bbaed873bc6f96b6ae532bf41234
Chris Brew <brewc@dy6nm2ng4q.legal.regn.net>
2024-03-12 02:04:27 -0400

    better metrics

--- chwra/lit_text.py
+++ chwra/lit_text.py
@@ -42,7 +42,7 @@ class DistilBertFineTune(LightningModule):
 
         self.log("train_loss", outputs.loss)
         preds = outputs.logits.argmax(dim=1)
-        acc = accuracy(preds, labels,task="multiclass",num_labels=5)
+        acc = accuracy(preds, labels,task="multiclass",num_classes=5)
         self.log("train_accuracy", acc)
 
     def validation_step(self, batch, batch_idx):
@@ -56,7 +56,7 @@ class DistilBertFineTune(LightningModule):
 
         self.log("eval_loss", outputs.loss)
         preds = outputs.logits.argmax(dim=1)
-        acc = accuracy(preds, labels,task="multiclass",num_labels=5)
+        acc = accuracy(preds, labels,task="multiclass",num_classes=5)
         self.log("eval_accuracy", acc)
         return preds
 
